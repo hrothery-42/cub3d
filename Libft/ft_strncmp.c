640a627d00426530f2916c40c6b5528489bbe7ef
Heather Rothery <hrothery@student.42wolfsburg.de>
2022-09-23 11:39:47 +0200

    ft_strcmp

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bvarlamo <bvarlamo@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*   By: hrothery <hrothery@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 19:54:06 by bvarlamo          #+#    #+#             */
-/*   Updated: 2021/07/12 12:45:29 by bvarlamo         ###   ########.fr       */
+/*   Updated: 2022/09/23 11:20:39 by hrothery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,20 @@ int	ft_strncmp(const char *s1, const char *s2, size_t size)
 	if (size == i)
 		i--;
 	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+}
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s1 && !s2)
+		return (0);
+	if (!s1)
+		return (-1);
+	if (!s2)
+		return (1);
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
+	return ((unsigned char) s1[i] - (unsigned char) s2[i]);
 }
